c76adbc1e7aa5bdb273dd2c2a8770e2d39adf54a
Nilber Silva <nilber@technoserv.com.br>
2019-11-21 18:10:50 -0300

    Initial Commit

---
+++ src/app/controllers/AvailableController.js
@@ -0,0 +1,63 @@
+import {
+  parseISO,
+  startOfDay,
+  endOfDay,
+  setHours,
+  format,
+  isAfter,
+} from 'date-fns';
+import { Op } from 'sequelize';
+import Appointments from '../models/Appointment';
+
+class AvailableController {
+  async index(req, res) {
+    const { date } = req.query;
+
+    if (!date) {
+      return res.status(400).json({ error: 'Invalid date' });
+    }
+
+    const searchDate = startOfDay(new Date(Number(date)));
+    const appointments = await Appointments.findAll({
+      where: {
+        provider_id: req.params.providerId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [searchDate, endOfDay(searchDate)],
+        },
+      },
+    });
+
+    const schedule = [
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+    ];
+
+    const newDate = new Date();
+    const available = schedule.map(time => {
+      const [hour] = time.split(':');
+      const value = setHours(searchDate, hour);
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        available:
+          isAfter(value, newDate) &&
+          !appointments.find(a => format(a.date, 'HH:mm') === time),
+      };
+    });
+
+    return res.json(available);
+  }
+}
+
+export default new AvailableController();
