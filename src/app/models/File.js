cb03858c41898f072c4aa302d898109ed75be141
Nilber Silva <nilber@technoserv.com.br>
2019-11-22 17:59:56 -0300

    Changed Files return URL

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -9,7 +9,7 @@ class File extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-            return `http://10.0.2.2:3333/files/${this.path}`;
+            return `https://gobarberapi.ddns.net/files/${this.path}`;
           },
         },
       },
