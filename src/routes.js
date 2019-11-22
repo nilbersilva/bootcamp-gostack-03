b7e64a9db926e891c3691f8f71672fbacc0ccfa1
Nilber Silva <nilber@technoserv.com.br>
2019-11-22 10:50:25 -0300

    Fix test route to get

--- src/routes.js
+++ src/routes.js
@@ -16,7 +16,7 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.put('/', (req, res) => res.send('ok'));
+routes.get('/', (req, res) => res.send('ok'));
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
